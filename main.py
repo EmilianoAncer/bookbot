6b1773c551740e7ba1e56880c095488354575961
Emiliano Ancer <emilianoancer@hotmail.com>
2024-07-22 14:50:39 -0600

    Added character count in main.py

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ def main():
         file_contects = f.read()
         split_content = file_contects.split()
         print(len(split_content))
+        char_count = {}
+        for char in file_contects:
+            lower_char = char.lower()
+            if lower_char not in char_count.keys():
+                char_count[lower_char] = 1
+            else:
+                char_count[lower_char] += 1
+
+
+        print(char_count)
 
 main()
