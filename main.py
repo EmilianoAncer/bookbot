ca52edf09075a0372d58bef2993f19ec0e828270
Emiliano Ancer <emilianoancer@hotmail.com>
2024-07-22 14:43:23 -0600

    Added word count in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contects = f.read()
-        print(file_contects)
+        split_content = file_contects.split()
+        print(len(split_content))
 
 main()
